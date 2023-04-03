9aeb0c39c70ff191e598283ed2d7c008d2e33b07
lale benedicto <larissagomes0119@gmail.com>
2023-04-03 17:46:35 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main(string[]args)
+    {
+        Console.WriteLine("----------SOMA DE INTEIROS---------");
+        Console.WriteLine("Digite o primeiro numero da soma: ");
+        int numero1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite o segundo numero da soma: ");
+        int numero2 = Convert.ToInt32(Console.ReadLine());
+        int soma = numero1 + numero2;
+
+        Console.WriteLine("O resultado da soma é: "+soma);
+        Console.ReadKey();  
+    }
+}
